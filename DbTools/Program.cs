cd5a7713c1e2673b69392e87b3626956a34a2c5d
Mohammad Dehghan <mohammadhd@systemgroup.net>
2020-12-20 02:32:47 +0330

    Add command line parameters for "restore" operation

--- DbTools/Program.cs
+++ DbTools/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
+using CommandLine;
 
 namespace DbTools
 {
@@ -8,12 +9,20 @@ namespace DbTools
     {
         static void Main(string[] args)
         {
-            var server = ".";
-            var connection = new SqlConnection($"server={server};database=master;integrated security=SSPI");
-            var backupFile = @"D:\SGIBS\Fixtures\DB0-Operations.bak";
-            var database = "DB0-Operations";
-            var dbFolder = @"D:\SGIBS\Data";
-            new DbOperations(connection).RestoreRahkaranDatabase(backupFile, database, dbFolder);
+            var parsed = Parser.Default.ParseArguments<RestoreOptions, object>(args);
+            parsed.WithParsed<RestoreOptions>(Restore);
+        }
+
+        static void Restore(RestoreOptions options)
+        {
+            string auth;
+            if (options.User != null)
+                auth = $"user={options.User};password={options.Password}";
+            else
+                auth = "integrated security=SSPI";
+            var connection = new SqlConnection($"server={options.Server};database=master;{auth}");
+            new DbOperations(connection)
+                .RestoreRahkaranDatabase(options.BackupFile, options.Database, options.OutputFolder);
         }
     }
 }
--- DbTools/RestoreOptions.cs
+++ DbTools/RestoreOptions.cs
@@ -1,20 +1,31 @@
 ﻿using CommandLine;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace DbTools
 {
-    [Verb("restore", isDefault:true, HelpText = "Restores a database backup.")]
+    [Verb("restore", isDefault: true, HelpText = "Restores a database backup.")]
     public class RestoreOptions
     {
+        public RestoreOptions(string server, string user, string password, string backupFile, string database, string outputFolder)
+        {
+            Server = server;
+            User = user;
+            Password = password;
+            BackupFile = backupFile;
+            Database = database;
+            OutputFolder = outputFolder;
+        }
+
         [Option('s', Required = true, HelpText = "The SQL Server instance to connect to.")]
         public string Server { get; }
-        [Option('u', Required = false, HelpText = "Login user name for the server. If omitted, Windows Authentication is used."]
+        [Option('u', Required = false, HelpText = "Login user name for the server. If omitted, Windows Authentication is used.")]
         public string User { get; }
         [Option('p', Required = false, HelpText = "Login password.")]
         public string Password { get; }
         [Option('b', Required = true, HelpText = "Path to the backup file to restore")]
         public string BackupFile { get; }
+        [Option('d', Required = true, HelpText = "The name of the database to create/restore.")]
+        public string Database { get; }
+        [Option('o', Required = true, HelpText = "The path to an existing directory to put the database files.")]
+        public string OutputFolder { get; }
     }
 }
